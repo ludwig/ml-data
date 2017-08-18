345af87e3faa91c707471d235ff37d2e7fcab4d5
Luis Armendariz <luis.armendariz@gmail.com>
2017-08-17 17:17:32 -0700

    Added Iris dataset

---
+++ iris/README.md
@@ -0,0 +1,7 @@
+
+# Iris flower data set
+
+* https://en.wikipedia.org/wiki/Iris_flower_data_set
+* http://archive.ics.uci.edu/ml/datasets/Iris
+
+
---
+++ iris/fetch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -x
+wget -nc http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data
+wget -nc http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.names
+
